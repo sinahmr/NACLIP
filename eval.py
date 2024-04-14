399b12ac5bdb74474f4900588165d42798d69457
Sina Hajimiri <sina.hajimiri@gmail.com>
2024-04-14 13:23:09 -0400

    Update visualization commands

--- eval.py
+++ eval.py
@@ -16,28 +16,21 @@ def parse_args():
     parser.add_argument('--std', default='')
     parser.add_argument('--pamr', default='')
     parser.add_argument('--work-dir', default='./work_logs/')
-    parser.add_argument('--show', action='store_true', help='show prediction results')
-    parser.add_argument('--show_dir', default='', help='directory to save visualization images')
+    parser.add_argument('--show-dir', default='', help='directory to save visualization images')
     args = parser.parse_args()
     return args
 
 
-def trigger_visualization_hook(cfg, args):
+def trigger_visualization_hook(cfg, show_dir):
     default_hooks = cfg.default_hooks
     if 'visualization' in default_hooks:
         visualization_hook = default_hooks['visualization']
-        # Turn on visualization
         visualization_hook['draw'] = True
-        if args.show:
-            visualization_hook['show'] = True
-            visualization_hook['wait_time'] = args.wait_time
-        if args.show_dir:
-            visualizer = cfg.visualizer
-            visualizer['save_dir'] = args.show_dir
+        visualizer = cfg.visualizer
+        visualizer['save_dir'] = show_dir
     else:
         raise RuntimeError(
-            'VisualizationHook must be included in default_hooks.'
-            'refer to usage '
+            'VisualizationHook must be included in default_hooks. refer to usage '
             '"visualization=dict(type=\'VisualizationHook\')"')
     cfg.model['pamr_steps'] = 50
     cfg.model['pamr_stride'] = [1, 2, 4, 8, 12, 24]
@@ -53,7 +46,6 @@ def main():
     args = parse_args()
 
     cfg = Config.fromfile(args.config)
-    cfg.launcher = 'none'
     cfg.work_dir = args.work_dir
 
     safe_set_arg(cfg, args.backbone, 'clip_path')
@@ -64,7 +56,7 @@ def main():
         cfg.model['pamr_steps'] = 0
     elif args.pamr == 'on':
         cfg.model['pamr_steps'] = 10
-    if args.show or args.show_dir != '':
+    if args.show_dir != '':
         trigger_visualization_hook(cfg, args)
 
     runner = Runner.from_cfg(cfg)
